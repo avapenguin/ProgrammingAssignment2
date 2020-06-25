3bbdcc7b0579f4acf815a2e9dc87c5a3151c5c8f
avapenguin <49593710+avapenguin@users.noreply.github.com>
2020-06-25 16:50:01 -0400

    Write solutions for cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2: Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+## Per the stated instructions, the following code 
+## contains two functions: one to create a "matrix" object
+## that can cache in its inverse, and the second to compute
+## the inverse of the special "matrix".
+
+## First, create the "matrix" object that can cache in its inverse:
 
 makeCacheMatrix <- function(x = matrix()) {
-
+            vers <- NULL
+            set <- function (y) {
+              x <<- y
+              vers <<- NULL
+            }
+            get <- function () x
+            setinv <- function(inverse) vers <<- inverse
+            getinv <- function () vers
+            list(set = set, get = get,
+                  setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Next, use the "solve" function to compute the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        vers <- x$getinv()
+        if (!is.null(vers)) {
+            message("getting cached data")
+            return(vers)
+        }
+        
+        mat <- x$get()
+        vers <- solve(mat, ...)
+        x$setinv(vers)
+        vers
 }
+
+##STOP! End of functions##
+
+##As a final step, test functions makeCacheMatrix and cacheSolve:
+
+#Test A: Test where inverse should exist:
+
+matrix_a <- makeCacheMatrix(matrix(data = 1:4, nrow = 2, ncol = 2))
+matrix_a$get()
+cacheSolve(matrix_a) #confirmed, inverse result is returned
+
+#Test B: Test where inverse does not exist (i.e. on a singular matrix):
+
+matrix_b <- makeCacheMatrix(matrix(data = c(3, 6, 5, 10), nrow = 2, ncol = 2))
+matrix_b$get()
+cacheSolve(matrix_b) #confirmed, error in solve.default as matrix is singular
